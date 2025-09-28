75feedc6ad6fc1d64dc23cdf28a631373e0a368f
aijia <liaijia@123.com>
2025-09-28 18:47:29 +0800

    小程序浏览业务代码

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,6 +2,7 @@ package com.sky.service;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
 
@@ -19,6 +20,20 @@ public interface DishService {
     DishVO getByIdWithFlavor(Long id);
 
     void updateWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
+
+    /**
+     * 根据菜品id查询菜品选项
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 
 }
 
