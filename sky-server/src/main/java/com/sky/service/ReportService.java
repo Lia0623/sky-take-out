28b54bcf2953a9893087b930152cb5d020702c88
aijia <liaijia@123.com>
2025-10-02 20:32:05 +0800

    导出运营数据代码

---
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,50 @@
+package com.sky.service;
+
+import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
+import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
+
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+
+public interface ReportService {
+
+    /**
+     * 统计指定时间区间的营业额
+     * @param begin
+     * @param end
+     * @return
+     */
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 统计指定时间区间的用户数据 总量和增量
+     * @param begin
+     * @param end
+     * @return
+     */
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 统计指定时间区段的订单数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+
+    /**
+     * 获取销量排名 top 10
+     * @param begin
+     * @param end
+     * @return
+     */
+    SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    void exportBusinessData(HttpServletResponse response);
+}
